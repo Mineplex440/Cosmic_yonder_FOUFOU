d5bf81b2091d67c9ebedf80df7cca985011c0981
Ethann <ethann.pencole29@gmail.com>
2024-05-06 15:50:57 +0200

    Start with Ncurses

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
+#include <ncurses.h>
+#include <stdlib.h>
+
 //test
+
+int main(){
+
+    char* ch = NULL; 
+
+    initscr();
+
+    printw("SALUT");
+
+    refresh();
+
+    getstr(ch);
+
+    printw("%s", ch);
+
+    getch();
+
+    endwin();
+
+}
